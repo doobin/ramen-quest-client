4af61d772b5273a8a547f0e141318b0681219eb8
doobin <doobin42@gmail.com>
2018-12-03 13:53:10 -0500

    Add icons to ListItem component   -display icon from FourSquare API
    Change CSS to render icons

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,6 +6,9 @@ class ListItem extends Component {
       <li className='listitem'
         onClick={() => this.props.handleListItemClick(this.props)}
       >
+        <img src={this.props.categories[0].icon.prefix+'32'+this.props.categories[0].icon.suffix}
+          alt={this.props.categories[0].name}
+        />
         {this.props.name}
       </li>
     )
