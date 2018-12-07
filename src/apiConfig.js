66226ae96891e2e7252db35faa1ba4b82731dbfc
doobin <doobin42@gmail.com>
2018-12-07 05:49:50 -0500

    Deploy on GitHub and Heroku   -Add Heroku url in apiConfig.js

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://radiant-brushlands-40574.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
