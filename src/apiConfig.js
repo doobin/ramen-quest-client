5068b690fcffb70237383dc0d36f915649e08995
doobin <doobin42@gmail.com>
2018-12-07 08:39:03 -0500

    Remove '/' from api url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://radiant-brushlands-40574.herokuapp.com/',
+  production: 'https://radiant-brushlands-40574.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
